f7d95e0189d069bf3599053c6d9c393431de3121
cedwics10 <cedric.von@outlook.fr>
2024-09-20 23:35:32 +0200

    f strings on template loader

--- utils/templates/TemplatesLoader.py
+++ utils/templates/TemplatesLoader.py
@@ -6,7 +6,7 @@ classes stored into the folder utils/template
 class TemplatesLoader:
     list = ["Dao", "Entity", 
             # "AdminCreateRoute",
-            "AdminReadRoute", 
+            "AdminReadRoute",
             "AdminDeleteRoute"]
 
     @classmethod
@@ -14,7 +14,7 @@ class TemplatesLoader:
         classes = []
 
         for template_name in TemplatesLoader.list:
-            module = __import__("utils.templates." + template_name, fromlist=[template_name])
+            module = __import__(f"utils.templates.{template_name}", fromlist=[template_name])
             classes.append(getattr(module, template_name))
         return classes
 
